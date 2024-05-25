9e5dfc4fb9950c3946bb8b0eb4b42abc1b48aa3e
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2024-05-25 17:44:28 -0600

    begin avenir::queue

--- avenir.h
+++ avenir.h
@@ -21,8 +21,80 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+#include <optional>
+#include <utility>
 
 namespace avenir
 {
+
+template <typename T>
+class queue
+{
+public:
+	queue() = default;
+	queue(const queue&) = delete;
+	//queue(queue&& oth) -TODO move constructor
+	//queue& operator=(queue&& oth) -TODO move assignment
 	
+	void push(const T& obj)
+	{
+		std::unique_lock<std::mutex> lock(m_mtx);
+		m_deque.push_back(obj);
+		lock.unlock();
+		m_cv.notify_one();
+	}
+	
+	template <typename... Args>
+	void emplace(Args&&... args)
+	{
+		std::unique_lock<std::mutex> lock(m_mtx);
+		m_deque.emplace(std::forward<Args>(args)...);
+		lock.unlock();
+		m_cv.notify_one();
+	}
+	
+	//immediately pop from the queue or return nullopt if empty
+	std::optional<T> pop()
+	{
+		std::optional<T> ret;
+		std::scoped_lock lock(m_mtx);
+		if(!m_deque.empty())
+		{
+			ret = std::move(m_deque.front());
+			m_deque.pop_front();
+		}
+		return ret;
+	}
+	
+	//immediately pop from the queue or wait until notified and pop
+	T wait()
+	{
+		std::unique_lock<std::mutex> lock(m_mtx);
+		if(m_deque.empty())
+		{
+			m_cv.wait(lock, [this]{ return !m_deque.empty(); });
+		}
+		T ret = std::move(m_deque.front());
+		m_deque.pop_front();
+		return ret;
+	}
+	
+	bool empty() const
+	{
+		return m_deque.empty();
+	}
+	
+	size_t size() const
+	{
+		return m_deque.size();
+	}
+private:
+	std::mutex m_mtx;
+	std::condition_variable m_cv;
+	std::deque<T> m_deque;
+};
+
 }
